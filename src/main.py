37c04f7f399723e17007a3df2ba445820ceabeec
Jake Salm <jake.salm05@gmail.com>
2026-09-16 00:11:09 -0400

    Add ByteTrack object tracking

--- src/main.py
+++ src/main.py
@@ -2,9 +2,12 @@ from ultralytics import YOLO
 
 model = YOLO("yolo26n.pt")
 
-# Run YOLO + ByteTrack on the webcam
-model.track(
+results = model.track(
     source=0,
     show=True,
-    tracker="custom_bytetrack.yaml"
+    tracker="custom_bytetrack.yaml",
+    stream=True
 )
+
+for result in results:
+    pass
